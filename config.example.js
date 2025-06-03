cecd6722f1088504563e98efd47b2a913c124ed9
kb-uipath <keith.born@uipath.com>
2025-06-03 16:42:22 -0400

    feat: add accounts view and local storage

---
+++ config.example.js
@@ -0,0 +1,6 @@
+/* Optional configuration for the CSM Dashboard.
+ * Rename this file to config.js to override defaults.
+ */
+// Change the local storage keys if desired
+window.STORAGE_KEY = 'csm_tasks';
+window.ACCOUNT_KEY = 'csm_accounts';
